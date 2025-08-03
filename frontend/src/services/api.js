33ffc0ddac0d88c884fc2ed80adebeb30eb94002
ArielJavierMurilloDLB <ariel.murillo@Jala.university>
2025-08-03 11:26:13 -0400

    add route backend 1.10

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://clon-gpt-eqafaxhcb4c3h7ch.canadacentral-01.azurewebsites.net/';
+const API_URL = process.env.REACT_APP_API_URL || 'https://clon-gpt-eqafaxhcb4c3h7ch.canadacentral-01.azurewebsites.net/api/';
 
 // Configuración global de axios
 const apiClient = axios.create({
