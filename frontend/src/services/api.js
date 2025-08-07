09dfb5832ab1d92238c245d08a1f588b347a942a
ArielJavierMurilloDLB <ariel.murillo@Jala.university>
2025-08-07 10:08:30 -0400

    Add url backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://clon-gpt-eqafaxhcb4c3h7ch.canadacentral-01.azurewebsites.net/api/';
+const API_URL = process.env.REACT_APP_API_URL || 'https://app-funtion-gnakajc6h7bkhyas.swedencentral-01.azurewebsites.net/api/';
 
 const apiClient = axios.create({
     baseURL: API_URL,
