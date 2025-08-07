a849d28e8781ea75d58fea4522e57bd8ab4b5820
ArielJavierMurilloDLB <ariel.murillo@Jala.university>
2025-08-07 09:47:42 -0400

    New changes about proyect

--- frontend/src/components/chat/ChatHeader.jsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from '@mui/material';
-
-const ChatHeader = ({ title }) => (
-  <div className="chat-header">
-    <Typography variant="h6">{title}</Typography>
-  </div>
-);
-
-export default ChatHeader;
